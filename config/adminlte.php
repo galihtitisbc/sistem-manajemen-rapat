5327fb54d3cf83d0a393256feb5c33248222e05a
galihtitisbc <hhonggil007@gmail.com>
2025-06-19 09:10:01 +0700

    add dual list box to change datatable

--- config/adminlte.php
+++ config/adminlte.php
@@ -14,9 +14,9 @@ return [
     |
     */
 
-    'title' => 'Aplikasi Modular Poliwangi',
-    'title_prefix' => '',
-    'title_postfix' => '',
+    'title'                                   => 'Aplikasi Modular Poliwangi',
+    'title_prefix'                            => '',
+    'title_postfix'                           => '',
 
     /*
     |--------------------------------------------------------------------------
@@ -30,8 +30,8 @@ return [
     |
     */
 
-    'use_ico_only' => true,
-    'use_full_favicon' => false,
+    'use_ico_only'                            => true,
+    'use_full_favicon'                        => false,
 
     /*
     |--------------------------------------------------------------------------
@@ -45,12 +45,12 @@ return [
     |
     */
 
-    'logo' => '',
-    'logo_img' => 'assets/img/navbar-brand.svg',
-    'logo_img_class' => 'brand-image ',
-    'logo_img_xl' => '',
-    'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'SIT',
+    'logo'                                    => '',
+    'logo_img'                                => 'assets/img/navbar-brand.svg',
+    'logo_img_class'                          => 'brand-image ',
+    'logo_img_xl'                             => '',
+    'logo_img_xl_class'                       => 'brand-image-xs',
+    'logo_img_alt'                            => 'SIT',
 
     /*
     |--------------------------------------------------------------------------
@@ -64,12 +64,12 @@ return [
     |
     */
 
-    'usermenu_enabled' => true,
-    'usermenu_header' => true,
-    'usermenu_header_class' => 'bg-primary',
-    'usermenu_image' => true,
-    'usermenu_desc' => false,
-    'usermenu_profile_url' => false,
+    'usermenu_enabled'                        => true,
+    'usermenu_header'                         => true,
+    'usermenu_header_class'                   => 'bg-primary',
+    'usermenu_image'                          => true,
+    'usermenu_desc'                           => false,
+    'usermenu_profile_url'                    => false,
 
     /*
     |--------------------------------------------------------------------------
@@ -83,12 +83,12 @@ return [
     |
     */
 
-    'layout_topnav' => null,
-    'layout_boxed' => null,
-    'layout_fixed_sidebar' => null,
-    'layout_fixed_navbar' => null,
-    'layout_fixed_footer' => null,
-    'layout_dark_mode' => null,
+    'layout_topnav'                           => null,
+    'layout_boxed'                            => null,
+    'layout_fixed_sidebar'                    => null,
+    'layout_fixed_navbar'                     => null,
+    'layout_fixed_footer'                     => null,
+    'layout_dark_mode'                        => null,
 
     /*
     |--------------------------------------------------------------------------
@@ -102,12 +102,12 @@ return [
     |
     */
 
-    'classes_auth_card' => '',
-    'classes_auth_header' => 'bg-gradient-info',
-    'classes_auth_body' => '',
-    'classes_auth_footer' => 'text-center',
-    'classes_auth_icon' => 'fa-lg text-info',
-    'classes_auth_btn' => 'btn-flat btn-primary',
+    'classes_auth_card'                       => '',
+    'classes_auth_header'                     => 'bg-gradient-info',
+    'classes_auth_body'                       => '',
+    'classes_auth_footer'                     => 'text-center',
+    'classes_auth_icon'                       => 'fa-lg text-info',
+    'classes_auth_btn'                        => 'btn-flat btn-primary',
 
     /*
     |--------------------------------------------------------------------------
@@ -121,17 +121,17 @@ return [
     |
     */
 
-    'classes_body' => '',
-    'classes_brand' => '',
-    'classes_brand_text' => '',
-    'classes_content_wrapper' => '',
-    'classes_content_header' => '',
-    'classes_content' => '',
-    'classes_sidebar' => 'sidebar-light-primary elevation-4',
-    'classes_sidebar_nav' => 'nav-compact',
-    'classes_topnav' => 'navbar-blue navbar-light',
-    'classes_topnav_nav' => 'navbar-expand',
-    'classes_topnav_container' => 'container',
+    'classes_body'                            => '',
+    'classes_brand'                           => '',
+    'classes_brand_text'                      => '',
+    'classes_content_wrapper'                 => '',
+    'classes_content_header'                  => '',
+    'classes_content'                         => '',
+    'classes_sidebar'                         => 'sidebar-light-primary elevation-4',
+    'classes_sidebar_nav'                     => 'nav-compact',
+    'classes_topnav'                          => 'navbar-blue navbar-light',
+    'classes_topnav_nav'                      => 'navbar-expand',
+    'classes_topnav_container'                => 'container',
 
     /*
     |--------------------------------------------------------------------------
@@ -145,15 +145,15 @@ return [
     |
     */
 
-    'sidebar_mini' => 'lg',
-    'sidebar_collapse' => false,
-    'sidebar_collapse_auto_size' => false,
-    'sidebar_collapse_remember' => false,
+    'sidebar_mini'                            => 'lg',
+    'sidebar_collapse'                        => false,
+    'sidebar_collapse_auto_size'              => false,
+    'sidebar_collapse_remember'               => false,
     'sidebar_collapse_remember_no_transition' => true,
-    'sidebar_scrollbar_theme' => 'os-theme-light',
-    'sidebar_scrollbar_auto_hide' => 'l',
-    'sidebar_nav_accordion' => true,
-    'sidebar_nav_animation_speed' => 300,
+    'sidebar_scrollbar_theme'                 => 'os-theme-light',
+    'sidebar_scrollbar_auto_hide'             => 'l',
+    'sidebar_nav_accordion'                   => true,
+    'sidebar_nav_animation_speed'             => 300,
 
     /*
     |--------------------------------------------------------------------------
@@ -167,13 +167,13 @@ return [
     |
     */
 
-    'right_sidebar' => false,
-    'right_sidebar_icon' => 'fas fa-cogs',
-    'right_sidebar_theme' => 'dark',
-    'right_sidebar_slide' => true,
-    'right_sidebar_push' => true,
-    'right_sidebar_scrollbar_theme' => 'os-theme-light',
-    'right_sidebar_scrollbar_auto_hide' => 'l',
+    'right_sidebar'                           => false,
+    'right_sidebar_icon'                      => 'fas fa-cogs',
+    'right_sidebar_theme'                     => 'dark',
+    'right_sidebar_slide'                     => true,
+    'right_sidebar_push'                      => true,
+    'right_sidebar_scrollbar_theme'           => 'os-theme-light',
+    'right_sidebar_scrollbar_auto_hide'       => 'l',
 
     /*
     |--------------------------------------------------------------------------
@@ -187,14 +187,14 @@ return [
     |
     */
 
-    'use_route_url' => false,
-    'dashboard_url' => 'home',
-    'logout_url' => 'logout',
-    'login_url' => 'login',
-    'register_url' => '',
-    'password_reset_url' => '',
-    'password_email_url' => '',
-    'profile_url' => 'users/profile',
+    'use_route_url'                           => false,
+    'dashboard_url'                           => 'home',
+    'logout_url'                              => 'logout',
+    'login_url'                               => 'login',
+    'register_url'                            => '',
+    'password_reset_url'                      => '',
+    'password_email_url'                      => '',
+    'profile_url'                             => 'users/profile',
 
     /*
     |--------------------------------------------------------------------------
@@ -208,9 +208,9 @@ return [
     |
     */
 
-    'enabled_laravel_mix' => false,
-    'laravel_mix_css_path' => 'css/app.css',
-    'laravel_mix_js_path' => 'js/app.js',
+    'enabled_laravel_mix'                     => false,
+    'laravel_mix_css_path'                    => 'css/app.css',
+    'laravel_mix_js_path'                     => 'js/app.js',
 
     /*
     |--------------------------------------------------------------------------
@@ -224,7 +224,7 @@ return [
     |
     */
 
-    'menu' => [
+    'menu'                                    => [
         // Navbar items:
         [
             'type'         => 'navbar-search',
@@ -236,22 +236,22 @@ return [
             'topnav_right' => true,
         ],
         // Sidebar items:
-		
+
         //['header' => 'account_settings'],
         //['header' => 'Menu'],
         [
-            'key' => 'dasbor',
-            'text' => 'Dasbor',
-            'url'  => 'home',
-            'icon' => 'nav-icon fas fa-tachometer-alt',
-			'active' => ['home', 'home*'],
+            'key'    => 'dasbor',
+            'text'   => 'Dasbor',
+            'url'    => 'home',
+            'icon'   => 'nav-icon fas fa-tachometer-alt',
+            'active' => ['home', 'home*'],
         ],
         [
-            'key' => 'bcl',
+            'key'  => 'bcl',
             'text' => '',
             'url'  => 'https://www.youtube.com/@bangcholik',
-            'icon' => '',			
-        ],       
+            'icon' => '',
+        ],
     ],
 
     /*
@@ -266,7 +266,7 @@ return [
     |
     */
 
-    'filters' => [
+    'filters'                                 => [
         JeroenNoten\LaravelAdminLte\Menu\Filters\GateFilter::class,
         JeroenNoten\LaravelAdminLte\Menu\Filters\HrefFilter::class,
         JeroenNoten\LaravelAdminLte\Menu\Filters\SearchFilter::class,
@@ -288,79 +288,94 @@ return [
     |
     */
 
-    'plugins' => [
-        'Datatables' => [
-			'active' => true,
-			'files' => [
-				[
-					'type' => 'js',
-					'asset' => true,
-					'location' => '/vendor/datatables/js/jquery.dataTables.min.js',
-				],
-				[
-					'type' => 'js',
-					'asset' => true,
-					'location' => '/vendor/datatables/js/dataTables.bootstrap4.min.js',
-				],
-				[
-					'type' => 'css',
-					'asset' => true,
-					'location' => '/vendor/datatables/css/dataTables.bootstrap4.min.css',
-				],
-			],
-		],
-        'Select2' => [
-            'active' => false,
-            'files' => [
+    'plugins'                                 => [
+        'Datatables'            => [
+            'active' => true,
+            'files'  => [
                 [
-                    'type' => 'js',
-                    'asset' => false,
+                    'type'     => 'js',
+                    'asset'    => true,
+                    'location' => '/vendor/datatables/js/jquery.dataTables.min.js',
+                ],
+                [
+                    'type'     => 'js',
+                    'asset'    => true,
+                    'location' => '/vendor/datatables/js/dataTables.bootstrap4.min.js',
+                ],
+                [
+                    'type'     => 'css',
+                    'asset'    => true,
+                    'location' => '/vendor/datatables/css/dataTables.bootstrap4.min.css',
+                ],
+            ],
+        ],
+        'Select2'               => [
+            'active' => false,
+            'files'  => [
+                [
+                    'type'     => 'js',
+                    'asset'    => false,
                     'location' => '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js',
                 ],
                 [
-                    'type' => 'css',
-                    'asset' => false,
+                    'type'     => 'css',
+                    'asset'    => false,
                     'location' => '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.css',
                 ],
             ],
         ],
-        'Chartjs' => [
+        'Chartjs'               => [
             'active' => false,
-            'files' => [
+            'files'  => [
                 [
-                    'type' => 'js',
-                    'asset' => false,
+                    'type'     => 'js',
+                    'asset'    => false,
                     'location' => '//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.bundle.min.js',
                 ],
             ],
         ],
-        'Sweetalert2' => [
+        'Sweetalert2'           => [
             'active' => true,
-            'files' => [
+            'files'  => [
                 [
-                    'type' => 'js',
-                    'asset' => true,
+                    'type'     => 'js',
+                    'asset'    => true,
                     'location' => '/vendor/sweetalert2/sweetalert2.min.js',
                 ],
                 [
-                    'type' => 'css',
-                    'asset' => true,
+                    'type'     => 'css',
+                    'asset'    => true,
                     'location' => '/vendor/sweetalert2/sweetalert2.min.css',
                 ],
             ],
         ],
-        'Pace' => [
+        'Pace'                  => [
             'active' => false,
-            'files' => [
+            'files'  => [
                 [
-                    'type' => 'css',
-                    'asset' => false,
+                    'type'     => 'css',
+                    'asset'    => false,
                     'location' => '//cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/themes/blue/pace-theme-center-radar.min.css',
                 ],
                 [
-                    'type' => 'js',
-                    'asset' => false,
+                    'type'     => 'js',
+                    'asset'    => false,
                     'location' => '//cdnjs.cloudflare.com/ajax/libs/pace/1.0.2/pace.min.js',
+                ],
+            ],
+        ],
+        'bootstrap4DualListbox' => [
+            'active' => true,
+            'files'  => [
+                [
+                    'type'     => 'css',
+                    'asset'    => true,
+                    'location' => '/vendor/bootstrap4-duallistbox/bootstrap-duallistbox.min.css',
+                ],
+                [
+                    'type'     => 'js',
+                    'asset'    => true,
+                    'location' => '/vendor/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js',
                 ],
             ],
         ],
@@ -379,23 +394,23 @@ return [
     |
     */
 
-    'iframe' => [
+    'iframe'                                  => [
         'default_tab' => [
-            'url' => null,
+            'url'   => null,
             'title' => null,
         ],
-        'buttons' => [
-            'close' => true,
-            'close_all' => true,
+        'buttons'     => [
+            'close'           => true,
+            'close_all'       => true,
             'close_all_other' => true,
-            'scroll_left' => true,
-            'scroll_right' => true,
-            'fullscreen' => true,
+            'scroll_left'     => true,
+            'scroll_right'    => true,
+            'fullscreen'      => true,
         ],
-        'options' => [
-            'loading_screen' => 1000,
+        'options'     => [
+            'loading_screen'    => 1000,
             'auto_show_new_tab' => true,
-            'use_navbar_items' => true,
+            'use_navbar_items'  => true,
         ],
     ],
 
@@ -411,5 +426,5 @@ return [
     |
     */
 
-    'livewire' => false,
+    'livewire'                                => false,
 ];
